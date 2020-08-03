61f6df62332b7e8034351e3e4edb0c40e76591f1
DarkWinther <DarkWinther7@gmail.com>
2020-08-03 11:58:39 +0200

    feat: part 1

--- server.js
+++ server.js
@@ -3,11 +3,21 @@ var app = express();
 var bodyParser = require('body-parser');
 var logger = require('morgan');
 
+app.set('view engine', 'pug');
+app.set('views', './views');
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(logger('common'));
 
-app.get('/', (req, res) => res.send('Hello world'));
+app.get('/', (req, res) => res.render('index', {
+  tekst: 'Min besked',
+  arr: [1, 2, 3]
+}));
+app.get('/test', (req, res) => res.render('index', {
+  arr: [1, 2, 3]
+}))
+
 app.get('*', (req, res) => res.send('404 Error'));
 
 app.listen(3000);
